994fdc70f8bebd2aa2dd478ec2dab4d8014b9f10
DiegoProgramadorJunior <diegoexequiel2@gmail.com>
2021-01-13 19:42:27 -0300

    Se insertan goles

--- ajax/php/insertarGolesPartidos.php
+++ ajax/php/insertarGolesPartidos.php
@@ -4,6 +4,6 @@ require_once '../../config/database.php';
 
 $resultado = false; 
 $database = Database::connect();
-$sql = 'INSERT INTO PARTIDOS_GOLES (ID_PARTIDO_FK,RUT_GOLEADOR_FK,ID_TIPO_GOL_FK,MINUTO_GOL,TIEMPO) VALUES ('.$_POST['idpartidofk'].','.$_POST['rutGoleador'].','.$_POST['idtipogol'].',"'.$_POST['min'].'","'.$_POST['tiempo'].'")';
+$sql = 'INSERT INTO PARTIDOS_GOLES (ID_PARTIDO_FK,RUT_GOLEADOR_FK,ID_TIPO_GOL_FK,MINUTO_GOL,TIEMPO) VALUES ('.$_POST['idpartidofk'].','.$_POST['rutGoleador'].','.$_POST['idtipogol'].',"'.$_POST['min'].'","'.$_POST['tiempo'].'"  )';
 $respuesta = $database->query($sql);
 echo $respuesta;
